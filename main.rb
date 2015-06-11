96c170406866283c23b14e9c65ad9fbb7991af2a
yoshihiro asada <yoshihiro0921@gmail.com>
2015-06-11 13:40:37 +0900

    Add conection with database

--- main.rb
+++ main.rb
@@ -1,6 +1,18 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(
+  "adapter" => "sqlite3",
+  "database" => "./bbs.db"
+)
+
+class Commnet < ActiveRecord::Base
+
+end
 
 get '/' do
+  @commets = Commnet.order("id desc").all
+  @title = "bbs"
   erb :index
 end
